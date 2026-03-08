09b8060ebc5420c5e31e8d0cc628a5019ecf6ab9
EnterpriseA1 <than.siriphiwat@gmail.com>
2026-03-08 22:38:41 +0700

    add Review schema and api route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const itemRoutes = require('./routes/items');
 const orderRoutes = require('./routes/orders');
+const reviewRoutes = require('./routes/reviews');
 require('dotenv').config();
 
 const app = express();
@@ -22,6 +23,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/auth', authRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/reviews', reviewRoutes);
 
 
 // สร้าง Route พื้นฐานไว้เทสว่า API ทำงานไหม
